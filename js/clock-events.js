130652e5050d203de82b2a24832356103f1f02e8
Sean Urgel <seantheurgel@gmail.com>
2018-07-18 11:47:01 +0800

    changed time for testing and display confetti

--- js/clock-events.js
+++ js/clock-events.js
@@ -10,21 +10,22 @@ function initEvents() {
 		var hours = date.getHours();
 
 		// Crying baby alarm @ 10:27
-		if (hours === 10 && minutes === 27 && babycry.paused) {
+		if (hours === 11 && minutes === 38 && babycry.paused) {
 			babycry.play();
 		}
 
 		// Pause baby crying @ 10:28
-		if (hours === 10 && minutes === 28 && !babycry.paused) {
+		if (hours === 11 && minutes === 39 && !babycry.paused) {
 			babycry.pause();
 			babycry.currentTime = 0;
 		}
 
-		if (hours === 11 && minutes === 4) {
+		// Latch GO! at @ 10:29
+
+		// When timer is at @11:30 hide clock and show countdown timer
+		if (hours === 11 && minutes === 42) {
 			document.getElementById("Clock").style.display = "none";
 			document.getElementById("CountdownTimer").style.display = "block";
-			document.getElementsByClassName("pp")[0].style.display = "inline";
-			document.getElementsByClassName("pp")[1].style.display = "inline";
 			clearInterval(interval);
 		}
 	}, 1000);
--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,4 @@
-var countDownDate =  new Date("July 18, 2018 10:59:00").getTime();
+var countDownDate =  new Date("July 18, 2018 11:43:00").getTime();
 
 var audio = new Audio('audio/woohoo.mp3');
 // audio.loop = true;
@@ -9,11 +9,13 @@ var x = setInterval(function(){
     var distance = countDownDate - now;
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("CountdownTimer").innerHTML = seconds + "s";
+    document.getElementById("CountdownTimer").innerHTML = seconds;
     if(distance < 0){
         clearInterval(x);
         var woohoo = " Woohoo! ";
         document.getElementById("CountdownTimer").innerHTML = woohoo;
+        document.getElementsByClassName("pp")[0].style.display = "inline";
+        document.getElementsByClassName("pp")[1].style.display = "inline";
         audio.play();
     }
-},1000)
+}, 1000)
